f0ca30b6238d3ea03a4b9b4bc49a45222de7514d
Anthony Martin <anthonymartin20@gmail.com>
2023-05-01 16:02:17 -0400

    Create Comment custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def self.update_comment_counter(post)
+    return unless post
+
+    count = Comment.where(post_id: post[:id]).count
+    post.update(comments_counter: count)
+  end
 end
