df5041b3939b83c6be8a37deb87490f915a01648
Anthony Martin <anthonymartin20@gmail.com>
2023-05-01 19:43:12 -0400

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comment
   has_many :like
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def self.last_posts(user)
     return unless user
 
