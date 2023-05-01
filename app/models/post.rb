cc05b6c7d640c3eaa1fb9fb66c9ba5591b52a6ba
Anthony Martin <anthonymartin20@gmail.com>
2023-05-01 15:50:22 -0400

    Create Post custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def self.last_comments(post)
+    Comment.where(post_id: post[:id]).order(created_at: :desc).limit(5)
+  end
+
+  def self.update_post_counter(user)
+    count = Post.where(author_id: user[:id]).count
+    user.update(posts_counter: count)
+  end
 end
