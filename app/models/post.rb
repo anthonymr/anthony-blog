0737c79598abd9e9d1e37d3a7012f28260aebfc8
Anthony Martin <anthonymartin20@gmail.com>
2023-05-01 16:03:09 -0400

    Add basic validation to methods parameters

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def self.last_comments(post)
+    return unless post
+
     Comment.where(post_id: post[:id]).order(created_at: :desc).limit(5)
   end
 
   def self.update_post_counter(user)
+    return unless user
+
     count = Post.where(author_id: user[:id]).count
     user.update(posts_counter: count)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   def self.last_posts(user)
+    return unless user
+
     Post.where(author_id: user[:id]).order(created_at: :desc).limit(3)
   end
 end
