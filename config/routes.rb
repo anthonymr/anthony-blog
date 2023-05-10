37536d049808116b04443072ef94f32175a0a4c8
Anthony Martin <anthonymartin20@gmail.com>
2023-05-10 14:01:25 -0400

    Flattern routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:create]
-    end
+    resources :posts, only: [:index, :show, :new, :create]
+  end
+
+  resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 
   root 'users#index'
