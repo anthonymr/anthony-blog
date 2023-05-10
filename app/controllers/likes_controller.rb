8a60c8b7501f4656947afbb560bb18fcbb8cb3dc
Anthony Martin <anthonymartin20@gmail.com>
2023-05-10 15:20:24 -0400

    Move update like counter log to model constructor

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     Like.create(post: @post, author: current_user)
-    Like.update_like_counter(@post)
     redirect_to user_posts_path(@post.author)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,11 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  def initialize(like_params)
+    super(like_params)
+    Like.update_like_counter(like_params[:post])
+  end
+
   def self.update_like_counter(post)
     post.update(likes_counter: post.likes.count)
   end
