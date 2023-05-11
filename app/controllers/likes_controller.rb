046c10c187b2afa6ddae4c991462039e6b42aa9f
Anthony Martin <anthonymartin20@gmail.com>
2023-05-11 04:16:33 -0400

    Improving performance

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
+    post = Post.includes(:likes).find(params[:post_id])
     likes = post.likes.where(author: current_user)
 
-    likes.any? ? likes.each(&:destroy) : Like.create(post:, author: current_user)
+    likes.any? ? likes.each(&:destroy) : Like.create(post: post, author: current_user)
 
     Like.update_like_counter(post)
     redirect_to user_posts_path(post.author)
