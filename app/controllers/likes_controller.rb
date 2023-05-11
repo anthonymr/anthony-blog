1beb502dfbc611593d6cae09dc35d91def78d5e2
Anthony Martin <anthonymartin20@gmail.com>
2023-05-11 03:56:11 -0400

    Allow one comment by user in one post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    Like.create(post: @post, author: current_user)
-    redirect_to user_posts_path(@post.author)
+    post = Post.find(params[:post_id])
+    likes = post.likes.where(author: current_user)
+
+    likes.any? ? likes.each(&:destroy) : Like.create(post: post, author: current_user)
+
+    Like.update_like_counter(post)
+    redirect_to user_posts_path(post.author)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,6 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  def initialize(like_params)
-    super
-    Like.update_like_counter(like_params[:post]) if like_params
-  end
-
   def self.update_like_counter(post)
     post.update(likes_counter: post.likes.count)
   end
