fb5cbbc47a1f45ab2c4cd13432358444e0f198c6
Anthony Martin <anthonymartin20@gmail.com>
2023-05-11 03:57:16 -0400

    Fixing rubocop offenses

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
     post = Post.find(params[:post_id])
     likes = post.likes.where(author: current_user)
 
-    likes.any? ? likes.each(&:destroy) : Like.create(post: post, author: current_user)
+    likes.any? ? likes.each(&:destroy) : Like.create(post:, author: current_user)
 
     Like.update_like_counter(post)
     redirect_to user_posts_path(post.author)
