0ec056d98d3e4011cdbfb63331cd1b61f54300f2
Anthony Martin <anthonymartin20@gmail.com>
2023-05-16 09:50:19 -0400

    Add email to User database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_174041) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_134459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_174041) do
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "comments", "posts"
